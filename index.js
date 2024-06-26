4fcbeb92ecf00ba77293d4610202060d9e64cc0f
Ryan945 <sarathryan@yahoo.com>
2024-06-25 21:25:59 -0700

    created html

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
-const gameBoard = document.querySelector("gameBoard");
-const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("#scoreText");
-const resetBtn = document.querySelector("reserBtn");
-const gameWidth = gameBoard.width;
-const gameHeight = gameBoard.height;
-const boardBackground = "white";
-const snakeColor = "lightgreen";
-const snakeBorder = "black";
-const foodColor = "red";
-const unitsize = 25;
-let running = false;
-let xVelocity = unitSize;
-let yVelocity = 0;
-let foodX;
-let foodY;
-let score = 0;
-let snake = [
-    {x:unitSize * 4, y:0},
-    {x:unitSize * 3, y:0},
-    {x:unitSize * 2, y:0},
-    {x:unitSize, y:0},
-    {x:0, y:0}
-];
+const gameBoard = document.querySelector("#gameBoard");
+if (gameBoard) {
+    const context = gameBoard.getContext("2d");
+    const scoreText = document.querySelector("#scoreText");
+    const resetBtn = document.querySelector("#resetBtn");
+    const gameWidth = gameBoard.width;
+    const gameHeight = gameBoard.height;
+    const boardBackground = "white";
+    const snakeColor = "lightgreen";
+    const snakeBorder = "black";
+    const foodColor = "red";
+    const unitSize = 25;  // corrected variable name to camelCase
+    let running = false;
+    let xVelocity = unitSize;
+    let yVelocity = 0;
+    let foodX;
+    let foodY;
+    let score = 0;
+    let snake = [
+        { x: unitSize * 4, y: 0 },
+        { x: unitSize * 3, y: 0 },
+        { x: unitSize * 2, y: 0 },
+        { x: unitSize, y: 0 },
+        { x: 0, y: 0 }
+    ];
 
-window.addEventListener("keydown", changeDirection);
-resetBtn.addEventListener("click", resetGame);
+    window.addEventListener("keydown", changeDirection);
+    resetBtn.addEventListener("click", resetGame);
 
-gameStart();
+    gameStart();
 
-function gameStart(){};
-function nextTick(){};
-function clearBoard(){};
-function createFood(){};
-function drawFood(){};
-function moveSnake(){};
-function drawSnake(){};
-function changeDirection(){};
-function checkGameover(){};
-function checkGameOver(){};
-function displayGameOver(){};
-function resetGame(){};
+    function gameStart() {
+        running = true;
+        scoreText.textContent = score;
+    }
+    function nextTick() { /* Add next tick logic */ }
+    function clearBoard() { /* Add clear board logic */ }
+    function createFood() {
+        function randomFood(min, max) {
+            const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+            return randNum;
+        }
+        foodX = randomFood(0, gameWidth - unitSize);
+        foodY = randomFood(0, gameHeight - unitSize);
+        console.log(foodX, foodY);
+    }
+    function drawFood() {
+        context.fillStyle = foodColor;
+        context.fillRect(foodX, foodY, unitSize, unitSize);
+    }
+    function moveSnake() { /* Add move snake logic */ }
+    function drawSnake() { /* Add draw snake logic */ }
+    function changeDirection(event) { /* Add change direction logic */ }
+    function checkGameover() { /* Add check game over logic */ }
+    function displayGameOver() { /* Add display game over logic */ }
+    function resetGame() { /* Add reset game logic */ }
+} else {
+    console.error("Game board element not found");
+}
